0eb457a2ca759a3444fe358056427206b78b15c6
Ammon Morris <amorris@apexclearing.com>
2017-03-21 15:30:49 -0700

    refactor(fileOrDirectory): remove grunt stuff

--- tasks/fileOrDirectory.js
+++ tasks/fileOrDirectory.js
@@ -1,49 +1,32 @@
-(function () {
+'use strict';
 
-    'use strict';
+var fs = require('fs-extra');
 
-    var _delete = function (grunt) {
-        return function (path) {
-            if (grunt.file.expand(path).length > 0) {
-                grunt.verbose.writeln('Deleting: \'' + grunt.file.expand(path) + '\'');
-                grunt.file.delete(grunt.file.expand(path), {force: true});
-            }
-        };
-    };
+function _delete (path) {
+    try {
+        console.log('Deleting: \'' + path + '\'');
+        fs.removeSync(path);
+    } catch (e) {
+        // Fail silently
+    }
+}
 
-    module.exports = {
-        _delete: _delete,
-        _copy: function (grunt) {
-            return function (source, destination) {
-                grunt.file.mkdir(destination);
-                grunt.file.expand(source).forEach(function (file) {
-                    if (grunt.file.isDir(file)) {
-                        grunt.file.recurse(file, function callback(abspath, rootdir, subdir, filename) {
-                            grunt.verbose.writeln('Copying: \'' + abspath + '\' to \'' + destination + '/' + (subdir ? subdir + '/' : '') + filename + '\'');
-                            grunt.file.copy(abspath, destination + '/' + (subdir ? subdir + '/' : '') + filename);
-                        });
-                    } else {
-                        grunt.file.copy(file, destination);
-                    }
-                });
-            };
-        },
-        _cleanUp: function (grunt) {
-            return function (options, allFiles) {
-                if (allFiles) {
-                    _delete(grunt)(options.working_directory);
-                    _delete(grunt)(options.working_directory + options.packaging_directory_name);
-                    _delete(grunt)(options.package_location + '*.tar.gz');
-                    _delete(grunt)(options.package_location + '*.build');
-                    _delete(grunt)(options.package_location + '*.changes');
-                    _delete(grunt)(options.package_location + '*.deb');
-                } else if (!grunt.option('verbose')) {
-                    _delete(grunt)(options.working_directory + options.packaging_directory_name);
-                    _delete(grunt)(options.package_location + '*.tar.gz');
-                    _delete(grunt)(options.package_location + '*.build');
-                }
-            };
-        }
-    };
+function _cleanUp (options, allFiles) {
+    if (allFiles) {
+        _delete(options.working_directory);
+        _delete(options.package_location + '*.tar.gz');
+        _delete(options.package_location + '*.build');
+        _delete(options.package_location + '*.changes');
+        _delete(options.package_location + '*.deb');
+    } else {
+        _delete(options.working_directory + options.packaging_directory_name);
+        _delete(options.package_location + '*.tar.gz');
+        _delete(options.package_location + '*.build');
+    }
+}
 
-})();
+module.exports = {
+    _delete: _delete,
+    _copy: fs.copySync,
+    _cleanUp: _cleanUp
+};
--- test/unit/fileOrDirectory_test.js
+++ test/unit/fileOrDirectory_test.js
@@ -1,120 +1,137 @@
-(function () {
+'use strict';
 
-    'use strict';
+var fs = require('fs-extra'),
+    testCase = require('nodeunit').testCase,
+    tmpDir = 'test/unit/tmp';
 
-    var sinon = require('sinon'),
-        testCase = require('nodeunit').testCase;
+exports.fileOrDirectory_test = {
+    'delete': testCase({
+        setUp: function (callback) {
+            this._delete = require('../../tasks/fileOrDirectory.js')._delete;
+            callback();
+        },
+        tearDown: function (callback) {
+            fs.removeSync(tmpDir);
+            callback();
+        },
+        'should delete existing file': function (test) {
+            // given
+            var path = tmpDir + "/test_file_to_delete.txt";
 
-    exports.fileOfDirectory_test = {
-        'delete': testCase({
-            setUp: function (callback) {
-                this.grunt = require('grunt');
-                this._delete = require('../../tasks/fileOrDirectory.js')._delete(this.grunt);
-                callback();
-            },
-            'should delete existing file': function (test) {
-                // given
-                var path = "test/unit/tmp/test_file_to_delete.txt";
+            fs.copySync("test/unit/test_file.txt", path);
 
-                this.grunt.file.copy("test/unit/test_file.txt", path);
+            var stats = fs.statSync(path);
 
-                // when
-                test.ok(this.grunt.file.exists(path), "File exists before being deleted");
-                this._delete(path);
+            // when
+            test.ok(stats.isFile(), "File exists before being deleted");
+            this._delete(path);
 
-                // then
-                test.ok(!this.grunt.file.exists(path), "File does not exist after being deleted");
-
-                // end
-                test.done();
-            },
-            'should delete existing empty directory': function (test) {
-                // given
-                var path = "test/unit/tmp/directory_to_delete";
-
-                this.grunt.file.mkdir(path);
-
-                // when
-                test.ok(this.grunt.file.exists(path), "Exists before being deleted");
-                test.ok(this.grunt.file.isDir(path), "Checking is directory before being deleted");
-                this._delete(path);
-
-                // then
-                test.ok(!this.grunt.file.exists(path), "Directory does not exist after being deleted");
+            // then
+            fs.access(path, function (err) {
+                test.equal(err.code, "ENOENT", "File does not exist after being deleted");
 
                 // end
                 test.done();
-            },
-            'should delete existing directory containing files': function (test) {
-                // given
-                var path = "test/unit/tmp/directory_with_files_to_delete";
+            });
+        },
+        'should delete existing empty directory': function (test) {
+            // given
+            var path = tmpDir + "/directory_to_delete";
 
-                this.grunt.file.mkdir(path);
-                this.grunt.file.copy("test/unit/test_file.txt", path + "/file_one.txt");
-                this.grunt.file.copy("test/unit/test_file.txt", path + "/file_two.txt");
+            fs.ensureDirSync(path);
 
-                // when
-                test.ok(this.grunt.file.exists(path), "Exists before being deleted");
-                test.ok(this.grunt.file.isDir(path), "Checking is directory before being deleted");
-                this._delete(path);
+            // when
+            var dirStats = fs.statSync(path);
+            test.ok(dirStats.isDirectory(), "Exists before being deleted");
+            test.ok(dirStats.isDirectory(), "Checking is directory before being deleted");
+            this._delete(path);
 
-                // then
-                test.ok(!this.grunt.file.exists(path), "File does not exist after being deleted");
+            // then
+            fs.access(path, function (err) {
+                test.equal(err.code, "ENOENT", "Directory does not exist after being deleted");
 
                 // end
                 test.done();
+            });
+        },
+        'should delete existing directory containing files': function (test) {
+            // given
+            var path = tmpDir + "/directory_with_files_to_delete";
+            fs.ensureDirSync(path);
+            fs.copySync("test/unit/test_file.txt", path + "/file_one.txt");
+            fs.copySync("test/unit/test_file.txt", path + "/file_two.txt");
+
+            // when
+            var dirStats = fs.statSync(path);
+            test.ok(dirStats.isDirectory(path), "Exists before being deleted");
+            test.ok(dirStats.isDirectory(path), "Checking is directory before being deleted");
+            this._delete(path);
+
+            // then
+            fs.access(path, function (err) {
+                test.equal(err.code, "ENOENT", "File does not exist after being deleted");
+
+                // end
+                test.done();
+            });
+        }
+    }),
+    'copy': testCase({
+        setUp: function (callback) {
+            this._copy = require('../../tasks/fileOrDirectory.js')._copy;
+            callback();
+        },
+        tearDown: function (callback) {
+            fs.removeSync(tmpDir);
+            callback();
+        },
+        'should copy directory and its containing files': function (test) {
+            // given
+            var directory = tmpDir + "/directory_to_copy",
+                sub_directory = directory + "/subdirectory",
+                files = [
+                        directory + "/file_one.txt",
+                        directory + "/file_two.txt",
+                        sub_directory + "/file_three.txt",
+                        sub_directory + "/file_four.txt"
+                ],
+                i = 0,
+                target_directory = tmpDir + "/target_directory",
+                target_sub_directory = target_directory + "/subdirectory",
+                target_files = [
+                        target_directory + "/file_one.txt",
+                        target_directory + "/file_two.txt",
+                        target_sub_directory + "/file_three.txt",
+                        target_sub_directory + "/file_four.txt"
+                ];
+
+            fs.ensureDirSync(sub_directory);
+
+            for (; i < files.length; i++) {
+                fs.copySync("test/unit/test_file.txt", files[i]);
             }
-        }),
-        'copy': testCase({
-            setUp: function (callback) {
-                this.grunt = require('grunt');
-                this._copy = require('../../tasks/fileOrDirectory.js')._copy(this.grunt);
-                callback();
-            },
-            'should copy directory and its containing files': function (test) {
-                // given
-                var directory = "test/unit/tmp/directory_to_copy",
-                    sub_directory = directory + "/subdirectory",
-                    files = [
-                            directory + "/file_one.txt",
-                            directory + "/file_two.txt",
-                            sub_directory + "/file_three.txt",
-                            sub_directory + "/file_four.txt"
-                    ],
-                    i = 0,
-                    target_directory = "test/unit/tmp/target_directory",
-                    target_sub_directory = target_directory + "/subdirectory",
-                    target_files = [
-                            target_directory + "/file_one.txt",
-                            target_directory + "/file_two.txt",
-                            target_sub_directory + "/file_three.txt",
-                            target_sub_directory + "/file_four.txt"
-                    ];
 
-                this.grunt.file.mkdir(directory);
-                this.grunt.file.mkdir(sub_directory);
-                for (; i < files.length; i++) {
-                    this.grunt.file.copy("test/unit/test_file.txt", files[i]);
-                }
+            // when
+            var dirStats = fs.statSync(directory);
+            test.ok(dirStats.isDirectory(), "Exists before being deleted");
+            test.ok(dirStats.isDirectory(), "Checking is directory before being deleted");
+            this._copy(directory, target_directory);
 
-                // when
-                test.ok(this.grunt.file.exists(directory), "Exists before being deleted");
-                test.ok(this.grunt.file.isDir(directory), "Checking is directory before being deleted");
-                this._copy(directory, target_directory);
+            // then
+            var targetStats = fs.statSync(target_directory);
+            test.ok(targetStats.isDirectory(), "Target directory created");
+            test.ok(targetStats.isDirectory(), "Target directory is a directory");
 
-                // then
-                test.ok(this.grunt.file.exists(target_directory), "Target directory created");
-                test.ok(this.grunt.file.isDir(target_directory), "Target directory is a directory");
-                test.ok(this.grunt.file.exists(target_sub_directory), "Target sub-directory created");
-                test.ok(this.grunt.file.isDir(target_sub_directory), "Target sub-directory is a directory");
-                for (i = 0; i < target_files.length; i++) {
-                    test.ok(this.grunt.file.exists(target_files[i]), "Target file created");
-                }
-
-                // end
-                test.done();
+            var subdirStats = fs.statSync(target_sub_directory);
+            test.ok(subdirStats.isDirectory(), "Target sub-directory created");
+            test.ok(subdirStats.isDirectory(), "Target sub-directory is a directory");
+            for (i = 0; i < target_files.length; i++) {
+                var targetFileStats = fs.statSync(target_files[i]);
+                test.ok(targetFileStats.isFile(), "Target file created");
             }
-        })
-    };
 
-})();
+            // end
+            test.done();
+        }
+    })
+};
