5249efec925487bcf7e86b96f613189fd26fa2f0
Ammon Morris <amorris@apexclearing.com>
2017-03-21 12:07:23 -0700

    refactor(options): replace grunt log with console log, move messages to messages.js

--- tasks/options.js
+++ tasks/options.js
@@ -1,66 +1,59 @@
-(function () {
+'use strict';
 
-    'use strict';
+var messages = require('./messages');
 
-    String.prototype.endsWidth = function (substring) {
-        var str = this;
-        if (str.lastIndexOf(substring) >= 0) {
-            return str.lastIndexOf(substring) === (str.length - substring.length);
+String.prototype.endsWidth = function (substring) {
+    var str = this;
+    if (str.lastIndexOf(substring) >= 0) {
+        return str.lastIndexOf(substring) === (str.length - substring.length);
+    }
+    return false;
+};
+
+function _validate (options, quiet) {
+    var valid = true;
+    if (!options.maintainer) {
+        if (!quiet) {
+            console.error(messages.noMaintainerDetails);
         }
-        return false;
-    };
-
-    module.exports = {
-        _validate: function (grunt) {
-            return function (options, quiet) {
-                var valid = true;
-                if (!options.maintainer) {
-                    if (!quiet) {
-                        grunt.log.subhead('no maintainer details provided!!');
-                        grunt.log.errorlns('please add the \'maintainer\' option specifying the name and email in your debian_package configuration in your Gruntfile.js or add \'DEBFULLNAME\' and \'DEBEMAIL\' environment variable (i.e. export DEBFULLNAME="James D Bloom" && export DEBEMAIL="jamesdbloom@email.com")');
-                    }
-                    valid = false;
-                }
-                if (options.maintainer && !options.maintainer.name) {
-                    if (!quiet) {
-                        grunt.log.subhead('no maintainer name provided!!');
-                        grunt.log.errorlns('please add the \'maintainer.name\' option in your debian_package configuration in your Gruntfile.js or add a \'DEBFULLNAME\' environment variable (i.e. export DEBFULLNAME="James D Bloom")');
-                    }
-                    valid = false;
-                }
-                if (options.maintainer && !options.maintainer.email) {
-                    if (!quiet) {
-                        grunt.log.subhead('no maintainer email provided!!');
-                        grunt.log.errorlns('please add the \'maintainer.email\' option in your debian_package configuration in your Gruntfile.js or add a \'DEBEMAIL\' environment variable (i.e. export DEBEMAIL="jamesdbloom@email.com")');
-                    }
-                    valid = false;
-                }
-                if (!options.short_description) {
-                    if (!quiet) {
-                        grunt.log.subhead('no short description provided!!');
-                        grunt.log.errorlns('please add the \'short_description\' option in your debian_package configuration in your Gruntfile.js or add a \'description\' field to package.json');
-                    }
-                    valid = false;
-                }
-                if (!options.long_description) {
-                    if (!quiet) {
-                        grunt.log.subhead('no long description provided!!');
-                        grunt.log.errorlns('please add the \'long_description\' option in your debian_package configuration in your Gruntfile.js or add a multi line \'description\' field to package.json (note: the first line is used as the short description and the remaining lines are used as the long description)');
-                    }
-                } else {
-                    // add extra space at start to ensure format is correct and allow simple unit test comparisons
-                    options.long_description = ' ' + options.long_description;
-                }
-                if (options.working_directory && !options.working_directory.endsWidth("/")) {
-                    options.working_directory = options.working_directory + "/";
-                }
-                if (valid) {
-                    options.package_name = options.package_name || (options.prefix || '') + (options.name || 'debian_package') + (options.postfix || '');
-                    options.package_location = options.working_directory + options.package_name;
-                }
-                return valid;
-            };
+        valid = false;
+    }
+    if (options.maintainer && !options.maintainer.name) {
+        if (!quiet) {
+            console.error(messages.noMaintainerName);
         }
-    };
+        valid = false;
+    }
+    if (options.maintainer && !options.maintainer.email) {
+        if (!quiet) {
+            console.error(messages.noMaintainerEmail);
+        }
+        valid = false;
+    }
+    if (!options.short_description) {
+        if (!quiet) {
+            console.error(messages.noShortDescription);
+        }
+        valid = false;
+    }
+    if (!options.long_description) {
+        if (!quiet) {
+            console.error(messages.noLongDescription);
+        }
+    } else {
+        // add extra space at start to ensure format is correct and allow simple unit test comparisons
+        options.long_description = ' ' + options.long_description;
+    }
+    if (options.working_directory && !options.working_directory.endsWidth("/")) {
+        options.working_directory = options.working_directory + "/";
+    }
+    if (valid) {
+        options.package_name = options.package_name || (options.prefix || '') + (options.name || 'debian_package') + (options.postfix || '');
+        options.package_location = options.working_directory + options.package_name;
+    }
+    return valid;
+}
 
-})();
+module.exports = {
+    _validate: _validate
+};
--- test/unit/options_test.js
+++ test/unit/options_test.js
@@ -1,124 +1,122 @@
-(function () {
+'use strict';
 
-    'use strict';
+// Exposes String.endsWidth
+require('../../tasks/options');
 
-    var sinon = require('sinon'),
-        testCase = require('nodeunit').testCase,
-        options = require('../../tasks/options'),
-        messages = require('../../tasks/messages');
+var sinon = require('sinon'),
+    testCase = require('nodeunit').testCase,
+    messages = require('../../tasks/messages');
 
-    exports.options_test = {
-        'endsWith string extension function': testCase({
-            'should match correct trailing substring': function (test) {
-                test.ok("abcdefabcabc".endsWidth("cabc"), '"abcdefabcabc" ends with "cabc"');
-                test.ok("abcdefabcabc".endsWidth("abc"), '"abcdefabcabc" ends with "abc"');
-                test.ok("abcdefabcabc".endsWidth("bc"), '"abcdefabcabc" ends with "bc"');
-                test.ok("abcdefabcabc".endsWidth("c"), '"abcdefabcabc" ends with "c"');
-                test.ok("abcdefabcabc".endsWidth("abcdefabcabc"), '"abcdefabcabc" ends with "abcdefabcabc"');
-                test.ok("abcdefabcabc".endsWidth(""), '"abcdefabcabc" ends with ""');
+exports.options_test = {
+    'endsWith string extension function': testCase({
+        'should match correct trailing substring': function (test) {
+            test.ok("abcdefabcabc".endsWidth("cabc"), '"abcdefabcabc" ends with "cabc"');
+            test.ok("abcdefabcabc".endsWidth("abc"), '"abcdefabcabc" ends with "abc"');
+            test.ok("abcdefabcabc".endsWidth("bc"), '"abcdefabcabc" ends with "bc"');
+            test.ok("abcdefabcabc".endsWidth("c"), '"abcdefabcabc" ends with "c"');
+            test.ok("abcdefabcabc".endsWidth("abcdefabcabc"), '"abcdefabcabc" ends with "abcdefabcabc"');
+            test.ok("abcdefabcabc".endsWidth(""), '"abcdefabcabc" ends with ""');
 
-                // end
-                test.done();
-            },
-            'should not-match incorrect trailing substring': function (test) {
-                test.ok(!"abcdefabcabc".endsWidth("ab"), '"abcdefabcabc" does not ends with "ab"');
-                test.ok(!"abcdefabcabc".endsWidth("abcd"), '"abcdefabcabc" does not ends with "abcd"');
-                test.ok(!"abcdefabcabc".endsWidth("def"), '"abcdefabcabc" does not ends with "def"');
-                test.ok(!"abcdefabcabc".endsWidth("cb"), '"abcdefabcabc" does not ends with "cb"');
+            // end
+            test.done();
+        },
+        'should not-match incorrect trailing substring': function (test) {
+            test.ok(!"abcdefabcabc".endsWidth("ab"), '"abcdefabcabc" does not ends with "ab"');
+            test.ok(!"abcdefabcabc".endsWidth("abcd"), '"abcdefabcabc" does not ends with "abcd"');
+            test.ok(!"abcdefabcabc".endsWidth("def"), '"abcdefabcabc" does not ends with "def"');
+            test.ok(!"abcdefabcabc".endsWidth("cb"), '"abcdefabcabc" does not ends with "cb"');
 
-                // end
-                test.done();
-            }
-        }),
-        'option validation': testCase({
-            setUp: function (callback) {
-                this._validateOptions = require('../../tasks/options.js')._validate;
-                sinon.spy(console, "error");
-                callback();
-            },
-            tearDown: function (callback) {
-                console.error.restore();
-                callback();
-            },
-            'should display errors for empty options': function (test) {
-                // given
-                var options = {};
+            // end
+            test.done();
+        }
+    }),
+    'option validation': testCase({
+        setUp: function (callback) {
+            this._validateOptions = require('../../tasks/options.js')._validate;
+            sinon.spy(console, "error");
+            callback();
+        },
+        tearDown: function (callback) {
+            console.error.restore();
+            callback();
+        },
+        'should display errors for empty options': function (test) {
+            // given
+            var options = {};
 
-                // when
-                var valid = this._validateOptions(options);
+            // when
+            var valid = this._validateOptions(options);
 
-                // then
-                test.ok(console.error.calledWith(messages.noMaintainerDetails));
-                test.ok(console.error.calledWith(messages.noShortDescription));
-                test.ok(console.error.calledWith(messages.noLongDescription));
+            // then
+            test.ok(console.error.calledWith(messages.noMaintainerDetails));
+            test.ok(console.error.calledWith(messages.noShortDescription));
+            test.ok(console.error.calledWith(messages.noLongDescription));
 
-                test.deepEqual(options, {});
-                test.ok(!valid, 'returns not valid');
+            test.deepEqual(options, {});
+            test.ok(!valid, 'returns not valid');
 
-                // end
-                test.done();
-            },
-            'should display no errors for correct options': function (test) {
-                // given
-                var options = {
-                    maintainer: {
-                        name: 'James D Bloom',
-                        email: 'jamesdbloom@email.com'
-                    },
-                    short_description: 'the short description',
-                    long_description: 'the long description added to the debian package',
-                    working_directory: 'dir'
-                };
+            // end
+            test.done();
+        },
+        'should display no errors for correct options': function (test) {
+            // given
+            var options = {
+                maintainer: {
+                    name: 'James D Bloom',
+                    email: 'jamesdbloom@email.com'
+                },
+                short_description: 'the short description',
+                long_description: 'the long description added to the debian package',
+                working_directory: 'dir'
+            };
 
-                // when
-                var valid = this._validateOptions(options);
+            // when
+            var valid = this._validateOptions(options);
 
-                // then
-                test.ok(console.error.notCalled);
-                test.deepEqual(options, {
-                    maintainer: {
-                        name: 'James D Bloom',
-                        email: 'jamesdbloom@email.com'
-                    },
-                    short_description: 'the short description',
-                    long_description: ' the long description added to the debian package',
-                    working_directory: 'dir/',
-                    package_name: 'debian_package',
-                    package_location: 'dir/debian_package'
-                });
-                test.ok(valid, 'returns valid');
+            // then
+            test.ok(console.error.notCalled);
+            test.deepEqual(options, {
+                maintainer: {
+                    name: 'James D Bloom',
+                    email: 'jamesdbloom@email.com'
+                },
+                short_description: 'the short description',
+                long_description: ' the long description added to the debian package',
+                working_directory: 'dir/',
+                package_name: 'debian_package',
+                package_location: 'dir/debian_package'
+            });
+            test.ok(valid, 'returns valid');
 
-                // end
-                test.done();
-            },
-            'should display when maintainer details not provided': function (test) {
-                // given
-                var options = {
-                    maintainer: {
-                    },
-                    short_description: 'the short description',
-                    long_description: 'the long description added to the debian package'
-                };
+            // end
+            test.done();
+        },
+        'should display when maintainer details not provided': function (test) {
+            // given
+            var options = {
+                maintainer: {
+                },
+                short_description: 'the short description',
+                long_description: 'the long description added to the debian package'
+            };
 
-                // when
-                var valid = this._validateOptions(options);
+            // when
+            var valid = this._validateOptions(options);
 
-                // then
-                test.ok(console.error.calledWith(messages.noMaintainerName));
-                test.ok(console.error.calledWith(messages.noMaintainerEmail));
+            // then
+            test.ok(console.error.calledWith(messages.noMaintainerName));
+            test.ok(console.error.calledWith(messages.noMaintainerEmail));
 
-                test.deepEqual(options, {
-                    maintainer: {
-                    },
-                    short_description: 'the short description',
-                    long_description: ' the long description added to the debian package'
-                });
-                test.ok(!valid, 'returns not valid');
+            test.deepEqual(options, {
+                maintainer: {
+                },
+                short_description: 'the short description',
+                long_description: ' the long description added to the debian package'
+            });
+            test.ok(!valid, 'returns not valid');
 
-                // end
-                test.done();
-            }
-        })
-    };
-
-})();
+            // end
+            test.done();
+        }
+    })
+};
