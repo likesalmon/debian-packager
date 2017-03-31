b22c3354546aca2547ce94c224c4ff9c8d4b8b26
Ammon Morris <amorris@apexclearing.com>
2017-03-31 09:18:31 -0700

    docs(messages.js): change debianPackagerConfig to debianPackagerOptions

--- tasks/messages.js
+++ tasks/messages.js
@@ -2,23 +2,27 @@
 
 module.exports = {
     noMaintainerDetails: 'No maintainer details provided!!\n' +
-        'Please add the `maintainer` option specifying the name and email in your package.json file under `debianPackagerConfig` or add `DEBFULLNAME` and `DEBEMAIL` environment variable (i.e. export DEBFULLNAME="James D Bloom" && export DEBEMAIL="jamesdbloom@email.com")',
+        'Please add the `maintainer` option specifying the name and email in your package.json file under `debianPackagerOptions` or add `DEBFULLNAME` and `DEBEMAIL` environment variable (i.e. export DEBFULLNAME="James D Bloom" && export DEBEMAIL="jamesdbloom@email.com")',
 
     noMaintainerName: 'No maintainer name provided!!\n' +
-        'please add the `maintainer.name` option in your package.json file under `debianPackagerConfig` or add a `DEBFULLNAME` environment variable (i.e. export DEBFULLNAME="James D Bloom")',
+        'please add the `maintainer.name` option in your package.json file under `debianPackagerOptions` or add a `DEBFULLNAME` environment variable (i.e. export DEBFULLNAME="James D Bloom")',
 
     noShortDescription: 'No short description provided!!\n' +
-        'Please add the `short_description` option in your package.json file under `debianPackagerConfig` or add a `description` field to package.json',
+        'Please add the `short_description` option in your package.json file under `debianPackagerOptions` or add a `description` field to package.json',
 
     noMaintainerEmail: 'No maintainer email provided!!\n' +
-        'Please add the `maintainer.email` option in your package.json file under `debianPackagerConfig` or add a `DEBEMAIL` environment variable (i.e. export DEBEMAIL="jamesdbloom@email.com")',
+        'Please add the `maintainer.email` option in your package.json file under `debianPackagerOptions` or add a `DEBEMAIL` environment variable (i.e. export DEBEMAIL="jamesdbloom@email.com")',
 
     noLongDescription: 'No long description provided!!\n' +
-        'Please add the `long_description` option in your package.json file under `debianPackagerConfig` or add a multi line `description` field to package.json (note: the first line is used as the short description and the remaining lines are used as the long description)',
+        'Please add the `long_description` option in your package.json file under `debianPackagerOptions` or add a multi line `description` field to package.json (note: the first line is used as the short description and the remaining lines are used as the long description)',
 
     providePackageJson: 'Please provide a valid package.json file, like `debian-packager package.json`',
 
     provideConfig: 'Please provide a config file, like `debian-packager package.json`',
+
+    provideFiles: 'Please provide a list of files in package.json under `debian-packager.files`. See the Overview section of the README for an example.',
+
+    provideSrc: 'Please provide a `src` property for each file. `src` is a list of glob patterns to match in your project. See the Overview section of the README for an example.',
 
     invalidOptions: 'Sorry, I can\'t create a package with invalid options.',
 
