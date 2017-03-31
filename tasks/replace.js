d4cd3fa69c40052fb772375b3d0ac10fbb3d1441
Ammon Morris <amorris@apexclearing.com>
2017-03-31 09:21:13 -0700

    refactor(replace): unwrap iffe

--- tasks/replace.js
+++ tasks/replace.js
@@ -1,38 +1,36 @@
-(function () {
 
-    'use strict';
+'use strict';
 
-    String.prototype.replaceAll = function (find, replace) {
-        var str = this;
-        return str.replace(new RegExp(find, 'g'), replace);
-    };
+String.prototype.replaceAll = function (find, replace) {
+    var str = this;
+    return str.replace(new RegExp(find, 'g'), replace);
+};
 
-    function findAndReplace (files, find, replace) {
-        console.log('Replacing: \'' + replace.replaceAll('\\n', '\\n').replaceAll('\\t', '\\t') + '\' for \'' + find.replaceAll('\\\\', '') + '\' in ' + files.join(' and '));
-        require('replace')({
-            regex: find,
-            replacement: replace,
-            paths: files,
-            recursive: true,
-            silent: true
-        });
-    }
+function findAndReplace (files, find, replace) {
+    console.log('Replacing: \'' + replace.replaceAll('\\n', '\\n').replaceAll('\\t', '\\t') + '\' for \'' + find.replaceAll('\\\\', '') + '\' in ' + files.join(' and '));
+    require('replace')({
+        regex: find,
+        replacement: replace,
+        paths: files,
+        recursive: true,
+        silent: true
+    });
+}
 
-    function transformAndReplace (files, find, list, transform) {
-        if (list) {
-            var replace = '';
+function transformAndReplace (files, find, list, transform) {
+    if (list) {
+        var replace = '';
 
-            for (var i = 0; i < list.length; i++) {
-                replace += transform(list[i]);
-            }
-
-            findAndReplace(files, find, replace);
+        for (var i = 0; i < list.length; i++) {
+            replace += transform(list[i]);
         }
+
+        findAndReplace(files, find, replace);
     }
+}
 
 
-    module.exports = {
-        _findAndReplace: findAndReplace,
-        _transformAndReplace: transformAndReplace
-    };
-})();
+module.exports = {
+    _findAndReplace: findAndReplace,
+    _transformAndReplace: transformAndReplace
+};
